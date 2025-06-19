47ee8d390d418348f400ee6cff1091f515da1f92
Parvez-hasan <parvezp7788@gmail.com>
2025-06-18 22:18:49 -0700

    first commit

---
+++ loop.js
@@ -0,0 +1,21 @@
+/* for (let i = 1; i <= 12; i++){
+    console.log('hello' + i);  
+} */
+for (i = 0; i < 10; i=i + 1){
+    console.log('number' + i);
+}
+
+
+let sum = 0;
+for ( i = 1; i < 15; i=i+1){
+   console.log(sum = sum + i);
+}
+console.log(sum);
+
+
+let names = ['Shimu', 'Saima', 'Shanta', 'Sumaya', 'Sweety', 'Sume',];
+
+for(i = 0; i <= names. length; i = i + 1){
+    console.log(names[i]);  
+}
+
